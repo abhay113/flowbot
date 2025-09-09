dd23eda486452b20a2d82b71a310b58e76297202
abhay113 <abhay@cateina.com>
2025-09-09 17:44:17 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to AA bot</h1>
+        <p>
+          This is an Account aggregator chatbot application.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
